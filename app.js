db64daa283de355490d23670c4c41424ab6a9130
souvikbanerjee86 <souvik.mlindia@gmail.com>
2018-07-22 10:46:56 +0530

    secondd commit

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,PUT,DELETE,OPTIONS');
+  next();
+});
 
 app.post('/api/images',(req,res,next)=>{
    console.log(req.body);
